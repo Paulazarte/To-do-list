153a1243a08d2f2deb940c05db25cba8f6c5c861
Paulazarte <paulazarte1997@gmail.com>
2020-11-10 00:26:52 -0300

    nuevos cambios

--- app.js
+++ app.js
@@ -1,124 +1,254 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+  formularioUI.reset();
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    formularioUI.reset();
+  }
+}
+
+//Efectos del Formulario
+
+var inputs = document.getElementsByClassName("formulario__input");
+for (var i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener("keyup", function () {
+    if (this.value.length >= 1) {
+      this.nextElementSibling.classList.add("fijar");
+    } else {
+      this.nextElementSibling.classList.remove("fijar");
+
+    }
+
+  })
+
+}
+
+
+//LOCALSTORAGE E INGRESO DE DATOS HACIA LA TABLA
+
 /*VARIABLES GLOBALES */
-const formUser = document.getElementById('formUser');
-const listCards = document.getElementById('card-list')
-let arrayCards = [];
+const formularioUI = document.querySelector('#formulario');
+const listaPeliculasUI = document.getElementById('tabla');
+let arrayPelis = [];
+
+
 
 /*FUNCIONES */
-const createCards = (title, textarea, classify) => {
-    let cards = {
-        title,
-        textarea,
-        classify,
-        state: false,
+const CrearPelis = (codigo, titulo, descripcion, categoria) => {
+  let pelis = {
+    codigo: codigo,
+    titulo: titulo,
+    descripcion: descripcion,
+    categoria: categoria,
+    estado: false
+  }
+
+  arrayPelis.push(pelis);
+
+  return pelis;
+
+}
+
+const GuardarDB = () => {
+  localStorage.setItem('peliculas', JSON.stringify(arrayPelis));
+
+  // PintarDB();
+
+}
+
+const PintarDB = () => {
+  //Esto limpia el HTML ¡!
+  // listaPeliculasUI.innerHTML = '';
+  arrayPelis = JSON.parse(localStorage.getItem('peliculas'));
+  if (arrayPelis === null) {
+    arrayPelis = [];
+  } else {
+    arrayPelis.forEach(element => {
+
+      if (element.estado) {
+        listaPeliculasUI.innerHTML += `
+  <tr>
+        <td style= "background-color:rgba(38, 226, 85, 0.4);">${element.codigo}</td> 
+        <td style= "background-color:rgba(38, 226, 85, 0.4);">${element.titulo}</td> 
+        <td style= "background-color:rgba(38, 226, 85, 0.4);">${element.descripcion}</td> 
+        <td style= "background-color:rgba(38, 226, 85, 0.4);">${element.categoria}</td> 
+        <td style= "background-color:rgba(38, 226, 85, 0.4);">
+          <span class="material-icons float-left mr-2"></span>
+          Estado - <b>${element.estado}</b>
+        </td>  
+        <td style= "background-color:rgba(38, 226, 85, 0.4);">
+          <span style= "cursor: pointer;" class="material-icons mr-3">delete</span> 
+          <span style= "cursor: pointer;" class="material-icons mr-3">edit</span>
+          <span style= "cursor: pointer;" class="material-icons mr-3">done_all</span>
+        </td>
+    </tr>`
+      } else {
+        listaPeliculasUI.innerHTML += `
+  <tr>
+        <td>${element.codigo}</td> 
+        <td>${element.titulo}</td> 
+        <td>${element.descripcion}</td> 
+        <td>${element.categoria}</td> 
+        <td>
+          <span class="material-icons float-left mr-2"></span>
+          Estado - <b>${element.estado}</b>
+        </td>  
+        <td>
+          <span style= "cursor: pointer;" class="material-icons mr-3">delete</span> 
+          <span style= "cursor: pointer;" class="material-icons mr-3">edit</span>
+          <span style= "cursor: pointer;" class="material-icons mr-3">done_all</span>
+        </td>
+  </tr>`
+      }
+
+    });
+  }
+}
+
+const EliminarDB = (texto) => {
+  let indexArray;
+  arrayPelis.forEach((elemento, index) => {
+    if (elemento.codigo === texto) {
+      indexArray = index;
+      console.log(elemento.codigo)
+      arrayPelis.splice(indexArray, 1);
     }
-
-    arrayCards.push(pelis);
-
-    return cards;
+  });
+  GuardarDB();
+  window.location.reload();
 }
 
-//Guardamos en LS
-const saveDB = () => {
-    localStorage.setItem('tasks', JSON.stringify(arrayCards));
+const ActivarDB = (texto) => {
+  // let indexArray = arrayPelis.findIndex((elemento) => {
+  //   return elemento.pelis === pelis
+  // });
+  // console.log(arrayPelis[indexArray]);
+  // arrayPelis[indexArray].estado = true;
+  // GuardarDB();
 
-}
-
-const printDB = () => {
-    arrayCards = JSON.parse(localStorage.getItem('tasks'));
-    if (arrayCards === null) {
-        arrayCards = [];
-    }else {
-        arrayCards.forEach(element => {
-            if (element.state) {
-                listCards.innerHTML += `
-            <div class="column">
-                <div class="card">${element.title.textarea.classify}</div>
-                <span style= "cursor: pointer;" class="material-icons mr-3">delete</span> 
-                <span style= "cursor: pointer;" class="material-icons mr-3">edit</span>
-                <span style= "cursor: pointer;" class="material-icons mr-3">done_all</span>
-            </div>`
-            }
-
-        });
+  let pelisArray = JSON.parse(localStorage.getItem('peliculas'));
+  pelisArray.forEach((elemento) => {
+    if (elemento.codigo == texto && elemento.estado == false) {
+      elemento.estado = true;
+      console.log('coincide: ' + elemento.estado);
+      localStorage.setItem('peliculas', JSON.stringify(pelisArray))
+      window.location.reload();
     }
+  })
+
 }
 
-const deleteDB = (text) => {
-    let indexArray;
-    arrayCards.forEach((elemento, index) => {
-        if (elemento.title === text) {
-            indexArray = index;
-            console.log(elemento.title)
-            arrayCards.splice(indexArray, 1);
+let editando = false;
+
+const EditarDB = (texto) => {
+  let pelisArray = JSON.parse(localStorage.getItem('peliculas'));
+  pelisArray.forEach((elemento) => {
+    if (elemento.codigo == texto) {
+      document.getElementById('codigo').value = elemento.codigo;
+      document.getElementById('codigo').setAttribute("disabled", "");
+      document.getElementById('titulo').value = elemento.titulo;
+      document.getElementById('descripcion').value = elemento.descripcion;
+      document.getElementById('categoria').value = elemento.categoria;
+      editando = true;
+    }
+  })
+  modal.style.display = "block";
+}
+
+
+//EVENTLISTENER
+formularioUI.addEventListener('submit', (e) => {
+  let enUso = false;
+  // e.preventDefault();
+  let codigoForm = document.querySelector('#codigo').value;
+  let tituloForm = document.querySelector('#titulo').value;
+  let descripcionForm = document.querySelector('#descripcion').value;
+  let categoriaForm = document.querySelector('#categoria').value;
+  if (codigoForm == "" || tituloForm == "" || descripcionForm == "" || categoriaForm == "") {
+    alert('Llene la totalidad de los campos')
+  } else {
+
+    let pelis = JSON.parse(localStorage.getItem('peliculas'));
+    if (pelis === null) {
+      pelis = [];
+    }
+    pelis.forEach((elemento) => {
+      if (elemento.codigo == codigoForm && editando == false) {
+        enUso = true;
+      }
+    });
+
+    if (enUso) {
+      alert('Este codigo ya está en uso')
+    } else if (editando) {
+      pelis.forEach((elemento) => {
+        if (elemento.codigo === codigoForm) {
+          elemento.categoria = categoriaForm;
+          elemento.descripcion = descripcionForm;
+          elemento.titulo = tituloForm;
+          localStorage.setItem('peliculas', JSON.stringify(pelis));
+          document.getElementById('codigo').removeAttribute("disabled");
         }
-    })
-    saveDB();
-    window.location.reload();
-}
+      })
+      formularioUI.reset();
+    } else {
+      CrearPelis(codigoForm, tituloForm, descripcionForm, categoriaForm);
+      GuardarDB();
+      formularioUI.reset();
+    }
+  }
 
-const activeDB = (text) => {
-    let cardArray.forEach((elemento) => {
-        if (elemento.)
-    })
-}
+});
+
+document.addEventListener('DOMContentLoaded', PintarDB);
+
+listaPeliculasUI.addEventListener('click', (e) => {
+
+  e.preventDefault();
+
+  if (e.target.innerHTML === 'done_all' || e.target.innerHTML === 'delete' || e.target.innerHTML === 'edit') {
+    // let texto = e.target.parentNode.parentNode.childNodes[2].innerText;
+    let texto = e.target.parentNode.parentNode.childNodes[1].innerText;
+    if (e.target.innerHTML === 'delete') {
+      //Acción de eliminar
+      console.log(e.target.innerHTML);
+      EliminarDB(texto);
+    }
+    if (e.target.innerHTML === 'done_all') {
+      //Acción de activar y desactivar
+      let texto = e.target.parentNode.parentNode.childNodes[1].firstChild.data;
+      console.log(e.target.parentNode.parentNode.childNodes[1].firstChild.data)
+      ActivarDB(texto)
+    }
+    if (e.target.innerHTML === 'edit') {
+      //Acción de abrir formulario
+      let texto = e.target.parentNode.parentNode.childNodes[1].innerText;
+      EditarDB(texto)
+    }
+  }
+});
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const titleInput = document.getElementById('title');
-// const textAreaInput = document.getElementById('textarea');
-// const classifyInput = document.getElementById('classify');
-
-// const generateId = function () {
-//     return '_' + Math.random().toString(36).substr(2, 9);
-// };
-
-// formUser.onsubmit = (e) => {
-//     const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-//     e.preventDefault();
-//     const title = titleInput.value;
-//     const textarea = textAreaInput.value;
-//     const classify = classifyInput.value;
-    
-//     tasks.push({
-//         title,
-//         textarea,
-//         classify,
-//         id: generateId().value,
-//         createdAT : Date.now()
-//     })
-    
-//     localStorage.setItem('tasks', JSON.stringify(tasks));
-    
-//     formUser.reset();
-//     displayTask();
-    
-// }
-
-// const loadForm = (taskId) => {
-//     const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-//     const task = tasks.find((u) => u.id === taskId);
-
-// }
 
 
 
